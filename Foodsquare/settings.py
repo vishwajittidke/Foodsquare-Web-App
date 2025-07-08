d58c075dc06c6b2fe293d6b23f523b05c4fe9083
Vishwajit Tidke <118745098+vishwajittidke@users.noreply.github.com>
2025-07-08 22:45:02 +0530

    Update settings.py

--- Foodsquare/settings.py
+++ Foodsquare/settings.py
@@ -100,7 +100,7 @@ EMAIL_HOST_PASSWORD = ''
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
