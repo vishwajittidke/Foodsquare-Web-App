6fa15b51331e46ed2be421f593d4f6f19b02a88e
Vishwajit Tidke <118745098+vishwajittidke@users.noreply.github.com>
2025-07-08 17:26:46 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
+# ✅ Load environment variables from a .env file
+dotenv.load_dotenv()
 
 def main():
+    # ✅ Set default settings module for Django
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Foodsquare.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,8 +18,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # ✅ Run the management command
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
