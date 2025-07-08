c6fc26e843115796e6c90e605ad742f241b24d8f
Vishwajit Tidke <118745098+vishwajittidke@users.noreply.github.com>
2025-07-08 17:28:12 +0530

    Update wsgi.py

--- Foodsquare/wsgi.py
+++ Foodsquare/wsgi.py
@@ -1,3 +1,22 @@
+# """
+# WSGI config for Foodsquare project.
+
+# It exposes the WSGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Foodsquare.settings')
+
+# application = get_wsgi_application()
+
+
+
 """
 WSGI config for Foodsquare project.
 
@@ -8,9 +27,15 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
+
+# ✅ Load environment variables from .env file
+dotenv.load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
+# ✅ Set the default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Foodsquare.settings')
 
+# ✅ Create the WSGI application
 application = get_wsgi_application()
